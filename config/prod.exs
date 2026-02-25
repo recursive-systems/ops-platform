7d1135f3f7cf00a8809e26a25bb1416fa0c63a14
Bradley Golden <bradleygolden@Bradleys-Mac-mini.local>
2026-02-24 20:23:29 -0600

    Disable force_ssl and fix config for Tailscale access
    - Commented out force_ssl config block completely
    
    - URL set to http://100.125.46.74:4000
    
    - IP binding set to all interfaces {0,0,0,0,0,0,0,0}
    
    - NOTE: Still investigating 301 redirect issue

--- config/prod.exs
+++ config/prod.exs
@@ -8,15 +8,13 @@ import Config
 config :ops_platform, OpsPlatformWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Force using SSL in production. This also sets the "strict-security-transport" header,
-# known as HSTS. If you have a health check endpoint, you may want to exclude it below.
-# Note `:force_ssl` is required to be set at compile-time.
-config :ops_platform, OpsPlatformWeb.Endpoint,
-  # force_ssl: [rewrite_on: [:x_forwarded_proto]],  # Disabled for Tailscale/internal use
-  exclude: [
-    # paths: ["/health"],
-    hosts: ["localhost", "127.0.0.1"]
-  ]
+# Force using SSL in production - DISABLED for Tailscale/internal use
+# config :ops_platform, OpsPlatformWeb.Endpoint,
+#   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+#   exclude: [
+#     paths: ["/health"],
+#     hosts: ["localhost", "127.0.0.1"]
+#   ]
 
 # Do not print debug messages in production
 config :logger, level: :info
